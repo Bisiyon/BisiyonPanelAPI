a2efaad885e1214e2ca36e8940a24351e31c8daa
Necati Kekeç <necatikekec09@gmail.com>
2025-06-23 00:08:37 +0300

    demirbas düzeltme

---
+++ BisiyonPanelAPI.Migration/Migrations/20250622210333_DemirbasFix.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BisiyonPanelAPI.Migration.Migrations
+{
+    /// <inheritdoc />
+    public partial class DemirbasFix : Microsoft.EntityFrameworkCore.Migrations.Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Serino",
+                table: "Demirbas",
+                newName: "SeriNo");
+
+            migrationBuilder.RenameColumn(
+                name: "Adi",
+                table: "Demirbas",
+                newName: "Ad");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SeriNo",
+                table: "Demirbas",
+                newName: "Serino");
+
+            migrationBuilder.RenameColumn(
+                name: "Ad",
+                table: "Demirbas",
+                newName: "Adi");
+        }
+    }
+}
