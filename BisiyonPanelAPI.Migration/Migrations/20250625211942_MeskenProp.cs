29d14494fec7f950071eb7791c4dadf9e9115b3a
BORA CETIN <73121904+boracetin@users.noreply.github.com>
2025-06-26 00:26:05 +0300

    katsayisi added

---
+++ BisiyonPanelAPI.Migration/Migrations/20250625211942_MeskenProp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+#nullable disable
+
+namespace BisiyonPanelAPI.Migration.Migrations
+{
+    /// <inheritdoc />
+    public partial class MeskenProp : Microsoft.EntityFrameworkCore.Migrations.Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KatSayisi",
+                table: "Mesken",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KatSayisi",
+                table: "Mesken");
+        }
+    }
+}
